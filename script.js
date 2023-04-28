a5c3bdb1fb72c67717e9e32b16f6aff87c63e9db
coolskeletons <jtlapech@terpmail.umd.edu>
2023-04-28 19:14:13 -0400

    MASSIVE STRIDES!!!

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
 
-function filterList(list, query) {
-    /*
-      Using the .filter array method, 
-      return a list that is filtered by comparing the item name in lower case
-      to the query in lower case
-      Ask the TAs if you need help with this
-    */
-    return list.filter((item)=>{
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = query.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery)
-    })
-  }
+
 
 
 async function mainEvent() {
     const mainForm = document.querySelector('.main_form');
-    const filterButton = document.querySelector('.filter_button');
 
     let currentList = [];
 
     mainForm.addEventListener('submit', async (buttonEvent) => {
         buttonEvent.preventDefault()
         console.log('search')
-        const results = await fetch('http://openlibrary.org/search.json?q=');
-        currentList = await results.json();
-        console.table(currentList); 
-    });
-
-    filterButton.addEventListener("click", (event)=>{
-        console.log('clicked filterButton');
-      
         const formData = new FormData(mainForm);
         const formProps = Object.fromEntries(formData);
-    
-        console.log(formProps);
-        const newList = filterList(currentList, formProps.resto);
-        
-        console.log(newList)
-        
-      })
+        console.log(formProps)
+        const results = await fetch(`http://openlibrary.org/search.json?q=${formProps['q']}`);
+        currentList = await results.json();
+        console.table(currentList); 
+        console.log(currentList)
+      
+        console.log(formProps)
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
